87fc8420f611d8501f58094baaedab11db5be6f3
StaceyO21 <staceyobrien21@gmail.com>
2024-12-03 21:42:21 -0600

    event controller

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,24 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String events(Model model) {
+        ArrayList events = new ArrayList<>();
+        events.add("Masqurade Ball");
+        events.add("Renfest");
+        events.add("Breaking Benjamin Concert");
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+}

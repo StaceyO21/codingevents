2d6c1fc0a201252bf3f37a1e94028f2d9b5ea5cf
StaceyO21 <staceyobrien21@gmail.com>
2025-02-03 22:12:18 -0600

    register form

---
+++ src/main/java/org/launchcode/codingevents/models/dto/RegisterFormDTO.java
@@ -0,0 +1,16 @@
+package org.launchcode.codingevents.models.dto;
+
+import com.mysql.cj.log.Log;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
